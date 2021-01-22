8d3d4f1b83b100a46f73791f27ba7cd3d73d3c5e
Nils Ramstoeck <nilsramstoeck@gmail.com>
2021-01-22 12:18:09 +0000

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+// Tic Online is an online multiplayer board game.
+// Copyright (C) 2021  Nils Ramstöck
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Tic Online</title>
+  <meta name="description" content="Tic Online Multiplayer Board Game">
+  <meta name="author" content="Nils Ramstöck">
+
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="js/scripts.js"></script>
+</head>
+<body>
+  <h1>Tic Online</h1>
+</body>
+</html>
